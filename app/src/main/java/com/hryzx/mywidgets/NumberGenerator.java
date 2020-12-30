a9de965c431dd2fa4b0dada61392e2645c843cec
harisfi <acc.cp.haris@gmail.com>
2020-12-30 23:21:57 +0700

    Initial commit

---
+++ app/src/main/java/com/hryzx/mywidgets/NumberGenerator.java
@@ -0,0 +1,10 @@
+package com.hryzx.mywidgets;
+
+import java.util.Random;
+
+public class NumberGenerator {
+    public static int Generate(int max) {
+        Random random = new Random();
+        return random.nextInt(max);
+    }
+}
